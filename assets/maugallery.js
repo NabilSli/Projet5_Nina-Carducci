19211eddd94a76943ad6820c09b27c6ee18b0efa
nabil Slimani <slimaninabil93350@gmail.com>
2023-08-03 15:33:38 +0200

    fix (maugallery.js): correct next image and previous image for the modal

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -138,14 +138,13 @@
       }
       let index = 0,
         next = null;
-
       $(imagesCollection).each(function (i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -179,7 +178,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
